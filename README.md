c846128a06fc49a4f9d7fc632ee5729ff5856400
Ignacio <ignacio@iPro-de-Ignacio.fibertel.com.ar>
2022-11-11 02:16:06 -0300

    first commit from CLI

---
+++ README.md
@@ -0,0 +1,11 @@
+# Simon
+
+This is a project where, despite there are lots of posts on web on it. Seemed challenging to build it by myself, tryng to give a nice and subtle look tribute to it. 
+
+Here I applied a carefull treatment of CSS in order to align perfectly each quarter of the SIMON PLATE, which was very defying.
+
+And, of course, writting the Js script added to make the game come alive... fluent con coherent with the traditional memory game.  I think this.game (if u try to build as I did without tutorials) is a good test to improve and consolidate DOM manipulation, functions, callstacks, timers, and loops.
+
+Hope u like my code...
+
+Thanks for reading.
---
+++ script.js
@@ -0,0 +1,97 @@
+"use strict";
+
+let gamePattern = [];
+let userPattern = [];
+const gameColors = ["green", "red", "yellow", "blue"];
+let level = 0;
+const sound = new Audio('green.mp3');
+const wrong = new Audio('wrong.mp3');
+
+//PLAYGAME
+function playGame() {
+  document.body.removeEventListener('keypress', playGame);
+  document.body.removeEventListener('click', playGame );
+  document.querySelector('#overlay').style.display = 'none';
+  level = 0;
+  gamePattern = [];
+  nextSequence();
+}
+//NEXT SEQUENCE
+const nextSequence = function () {
+  document.querySelector(
+    "h2"
+  ).innerHTML = `<h2 class="detach">LEVEL ${level}</span></h2>`;
+  userPattern = [];
+  let index = Math.round(Math.random() * 3);
+  let randomChosenColour = gameColors[index];
+  gamePattern.push(randomChosenColour);
+  animatePress(randomChosenColour);
+  for (let button of gameColors) {
+    document.querySelector(`.${button}`).addEventListener("click", userChoice);
+  }
+};
+function animatePress(buttoncolor) {
+  let button = document.querySelector(`#${buttoncolor}`);
+  button.classList.remove("hidden");
+  sound.play();
+  setTimeout(() => button.classList.add("hidden"), 200);
+}
+
+function userChoice(e) {
+  let colorClicked = e.target.classList[1];
+  animatePress(colorClicked);
+  userPattern.push(colorClicked);
+  checkAnswer(userPattern.length - 1);
+}
+
+//CHECK
+function checkAnswer(currentlevel) {
+  if (userPattern[currentlevel] === gamePattern[currentlevel]) {
+    if (userPattern.length === gamePattern.length) {
+      level++;
+      setTimeout(() => {
+        previousPattern();
+      }, 500);
+    }
+  } else {
+    return gameOver();
+  }
+}
+//GAMEOVER
+function gameOver() {
+  wrong.play();
+  //display an overlay GAME OVER SETUP.
+  console.log('u lost')
+  
+  document.querySelector('#overlay').style.display = "block";
+
+  //add eventlistener to restart the game
+  document.body.addEventListener('keypress', playGame);
+  setTimeout( () => {
+    document.body.addEventListener('click', playGame)
+  }, 500
+
+  )
+  
+}
+//START
+document.body.addEventListener("keypress", playGame);
+document.querySelector('#start').addEventListener("click", playGame);
+
+function previousPattern() {
+  for (let button of gameColors) {
+    document
+      .querySelector(`.${button}`)
+      .removeEventListener('click', userChoice);
+  }
+  //loop over the gamePatter in order to show the iteration 0.5 second gap.
+  for (let i = 1; i <= gamePattern.length; i++) {
+    setTimeout(() => {
+      animatePress(gamePattern[i - 1]);
+    }, 1000 * i);
+    console.log(gamePattern.length, gamePattern);
+    if (i === gamePattern.length) {
+      return setTimeout(nextSequence, 1000 * (i + 1));
+    }
+  }
+}
